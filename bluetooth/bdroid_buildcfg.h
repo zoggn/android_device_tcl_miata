8f0522005edd1795628a0fbbc0b9709cf4645b87
Nikita Kurkov <kurkov.nikita@gmail.com>
2017-03-02 22:03:55 +0500

    Try to fix bt

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -26,5 +26,7 @@
 #define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
+/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
+#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE 
 #define BLE_PERIPHERAL_ADV_NAME  FALSE
 #endif
