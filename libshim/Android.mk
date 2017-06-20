883f6530ce1b58ab8ed282a7dfc84fa91f213fee
zoggn <kurkov.nikita@gmail.com>
2017-06-20 20:39:49 +0300

    add shim for camera

--- libshim/Android.mk
+++ libshim/Android.mk
@@ -22,3 +22,29 @@ LOCAL_MODULE := libshim_rmt_storage
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+# alcatel_camera
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+     VectorImpl.c
+
+LOCAL_SHARED_LIBRARIES :=
+LOCAL_MODULE := libshim_tcl_camera
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := shim_libwvm.c
+
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_MODULE := libshim_libwvm
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
